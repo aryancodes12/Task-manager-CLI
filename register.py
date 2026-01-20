1153e44247a6f1d84eca4646a6e39d35a9f49f0e
aryancodes12 <9040aryangupta@gmail.com>
2026-01-20 22:47:06 +0530

    refining the logic from beginning

--- register.py
+++ register.py
@@ -1,53 +1,62 @@
 import json
+from rich.console import Console
 
+console = Console()
+
+#Loading the DATA from JSON
 file_name = "user_data.json"
 try:
     with open(file_name) as f:
         user_info = json.load(f)
 except FileNotFoundError:
-    print("File does not exist")
+    console.print("File does not exist", style="bold red")
 else:
     print("Previous data is loaded")
 
+#Tracking the current user id from the JSON file
 user_info = {}
-counter = 1
-while True:
+if user_info:
+    counter = max(int(uid[1:]) for uid in user_info.keys()) + 1
+else:
+    counter = 1
+
+
+
+#Registeration main fuction
+def user_registeration():
+    console.print("Registeration Section", style="bold cyan")
+    first_name = input("Enter First Name:").title()
+    last_name = input("Enter Last Name:").title()
+    email_id = input("Enter email id:")
+    username = input("Create username:")
+    password = input("Create a password:")
+    re_enter = input("Re-Enter the password:")
+
+    #Adding the DATA in Dict user_info
+
     user_id = f"u{counter:03d}"
+    user_info[user_id] = {
+        "First_Name": first_name,
+        "Last_Name": last_name,
+        "Email_id": email,
+        "Username": username,
+        "Password": re_enter,
+        }
+    
 
-    def user_register():
-        print("\n\033[35mRegister Here\033[0m\n")
-        first_name = input("Enter your First name: ").title()
-        last_name = input("Enter your Last name: ").title()
-        email = input("Enter your Email-Id: ")
-        username = input("Create your username: ")
-        is_password = True
-        password = input("Create password: ")
-        while is_password:
-            if password == username:
-                print("\033[1;91mUsername and password cannot be same.\033[0m")
-                password = input("Try creating again: ")
-            elif len(password) < 8:
-                print("Password should be greater than 8 character")
-                password = input("Enter password: ")
-            else:
-                re_enter = input("Enter password again: ")
-                is_password = False 
-        user_info[user_id] = {
-            "First_Name": first_name,
-            "Last_Name": last_name,
-            "Email_Id": email,
-            "Username": username,
-            "Password": re_enter,
-            }
-        print("Registeration Successful")
-
-
+#Asking for permission to begin registeration
+active = True
+while active:
     register = input("Enter 'y/n' for registeration: ").lower()
     if register == "y":
-        user_register()
-        counter += 1
+        user_registeration()
     elif register == "n":
-        break
+        active = False
+
+
+
+
+
 
 try:
     with open(file_name, "w") as f:
@@ -57,9 +66,6 @@ except FileNotFoundError:
 else:
     print("DATA IS SAVED")
 
-print("\nUser DATA:\n")
-for uid, infos in user_info.items():
-    print(f"\033[1;92mUser ID->\033[0m {uid}")
-    for k, v in infos.items():
-        print(f"\t{k} ->\033[1;93m{v}\033[0m")
-        print()
+print("User DATA:")
+for k, v in user_info.items():
+    print(k, ":", v)
